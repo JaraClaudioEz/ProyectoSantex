b5c788a7dc465cd92048436f3633d35431f83b0e
JaraClaudioEz <claudiojara848@gmail.com>
2022-10-01 20:41:27 -0300

    Corregido errores back sequelize

--- back/models/user.models.js
+++ back/models/user.models.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: true,
     tableName: 'users',
   });
-  user.tipousuario = user.belongTo(tipousuario);
+  //user.tipousuario = user.belongTo(tipousuario);
 
   return user;
 };
